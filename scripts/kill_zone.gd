05049636fd33c127f21780f9134f68cb08c5f225
M3dvidek <medard@cechovi.org>
2025-06-05 17:45:25 +0200

    More platform movement and text(fix text blur and pixelation)

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -3,13 +3,17 @@ extends Area2D
 @onready var timer: Timer = $Timer
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "Player-Knight" && timer.time_left == 0):
 		print("You died") # Replace with function body.
 		body.die()
+		await get_tree().create_timer(1.7).timeout
 		audio_stream_player_2d.play()
 		timer.start()
 
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+
+	
