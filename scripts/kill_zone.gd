ddc8c39749031b1a8963817f358c4a879529df4a
Atrindade23 <atrindadetnt231196@gmail.com>
2025-06-12 17:29:21 -0300

    fixed KZ

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -2,10 +2,11 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
-
+var is_dead = false
 
 func _on_body_entered(body: Node2D) -> void:
-	if (body.name == "Player-Knight" && timer.time_left == 0):
+	if (body.is_in_group("Player") && timer.time_left == 0) && is_dead != true:
+		is_dead = true
 		print("You died") # Replace with function body.
 		body.die()
 		await get_tree().create_timer(0.9).timeout
--- scripts/player_knight.gd
+++ scripts/player_knight.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 
+var is_dead = false
 @export var walk_speed = 150
 @export var run_speed = 250
 var speed
@@ -15,49 +16,51 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_force
+	if (is_dead == false):
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = jump_force
+			
+		# Jump deceleration
+		if Input.is_action_just_released("jump") and velocity.y < 0:
+			velocity.y *= jump_deceleration
+			
+		# Input direction
+		var direction := Input.get_axis("go_left", "go_right")
 		
-	# Jump deceleration
-	if Input.is_action_just_released("jump") and velocity.y < 0:
-		velocity.y *= jump_deceleration
-		
-	# Input direction
-	var direction := Input.get_axis("go_left", "go_right")
-	
-	# Run movement
-	if Input.is_action_pressed("run"):
-		speed = run_speed
-	else:
-		speed = walk_speed
+		# Run movement
+		if Input.is_action_pressed("run"):
+			speed = run_speed
+		else:
+			speed = walk_speed
 	
 	# Aplly movement
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		
-		# flip sprite
-	if direction < 0:
-		animated_sprite.flip_h = true
-	elif direction > 0:
-		animated_sprite.flip_h = false
-	
-	#add animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("IDLE")
+		if direction:
+			velocity.x = direction * speed
 		else:
-			animated_sprite.play("RUNNING")
-	else:
-		animated_sprite.play("IDLE")
+			velocity.x = move_toward(velocity.x, 0, speed)
+			
+			# flip sprite
+		if direction < 0:
+			animated_sprite.flip_h = true
+		elif direction > 0:
+			animated_sprite.flip_h = false
+	
+		#add animations
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("IDLE")
+			else:
+				animated_sprite.play("RUNNING")
+		else:
+			animated_sprite.play("IDLE")
 		
 	move_and_slide()
 	
 func die():
+		is_dead = true
+		velocity.x = 0
 		animated_sprite.play("HIT") #temporary
-		set_physics_process(false)
 		
 func _on_animated_sprite_2d_animation_finished():
 	if animated_sprite.animation == "HIT":
